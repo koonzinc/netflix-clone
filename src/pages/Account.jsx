e9bf385757798738107ca03045b8873755c9086b
koonzinc <koonz.eth@gmail.com>
2022-09-06 11:12:46 -0400

    fix

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -8,7 +8,7 @@ const Account = () => {
         <img className='w-full h-[400px] object-cover' src="https://miro.medium.com/max/1400/1*5lyavS59mazOFnb55Z6znQ.png" alt="/" />
         <div className='bg-black/60 fixed top-0 left-0 w-full h-[550px]'>
           <div className='absolute top-[20%] p-4 md:p-8'>
-            <h1 className='text-3xl md:text-5xl font-bold'>M Shows</h1>
+            <h1 className='text-3xl md:text-5xl font-bold'>My Shows</h1>
           </div>
         </div>
       </div>
