62625ee1004672b24f48e887fc96357bd5849769
koonzinc <koonz.eth@gmail.com>
2022-09-05 20:41:42 -0400

    all

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,51 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { UserAuth } from '../context/AuthContext';
 
 const Navbar = () => {
-  return (
-    <div className='flex items-center justify-between p-4 z-[100] absolute w-full'>
-        <Link to='/'>
-            <h1 className='text-red-600 text-4xl font-bold cursor-pointer'>NETFLIX</h1>
-        </Link>
-        <div>
-            <Link to='/login'>
-                <button className='text-white pr-4'>
-                    Sign In
-                </button>
+    const { user, logOut } = UserAuth()
+    const navigate = useNavigate()
+
+    const handleLogout = async () => {
+        try {
+            await logOut();
+            navigate('/');
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+
+    return (
+        <div className='flex items-center justify-between p-4 z-[100] absolute w-full'>
+            <Link to='/'>
+                <h1 className='text-red-600 text-4xl font-bold cursor-pointer'>NETFLIX</h1>
             </Link>
-            <Link to='/signup'>
-                <button className='bg-red-600 px-6 py-2 rounded cursor-pointer text-white hover:rounded-lg duration-300 hover:bg-red-500'>
-                    Sign Up
+            {user?.email ? <div>
+                <Link to='/account'>
+                    <button className='text-white pr-4'>
+                        Account
+                    </button>
+                </Link>
+
+                <button onClick={handleLogout} className='bg-red-600 px-6 py-2 rounded cursor-pointer text-white hover:rounded-lg duration-300 hover:bg-red-500'>
+                    Logout
                 </button>
-            </Link>
+
+            </div> : <div>
+                <Link to='/login'>
+                    <button className='text-white pr-4'>
+                        Sign In
+                    </button>
+                </Link>
+                <Link to='/signup'>
+                    <button className='bg-red-600 px-6 py-2 rounded cursor-pointer text-white hover:rounded-lg duration-300 hover:bg-red-500'>
+                        Sign Up
+                    </button>
+                </Link>
+            </div>}
         </div>
-    </div>
-  )
+    )
 }
 
 export default Navbar
