ba3aa8187b5aea6b4de0308f1c2591f8b08e9b8f
koonzinc <koonz.eth@gmail.com>
2022-09-05 20:06:25 -0400

    fix

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,28 +1,40 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import Movie from './Movie';
+import {MdChevronLeft, MdChevronRight} from 'react-icons/md';
 
-const Row = ({ title, fetchURL }) => {
-const [movies, setMovies] = useState([])
+const Row = ({ title, fetchURL, rowID }) => {
+    const [movies, setMovies] = useState([])
+   
 
-useEffect(() => {
-    axios.get(fetchURL).then((response) => {
-        setMovies(response.data.results)
-    })
-}, [fetchURL])
+    useEffect(() => {
+        axios.get(fetchURL).then((response) => {
+            setMovies(response.data.results)
+        })
+    }, [fetchURL])
 
-console.log(movies)
+    const slideLeft = () => {
+        let slider = document.getElementById('slider' + rowID);
+        slider.scrollLeft = slider.scrollLeft - 500;
+    }
+    const slideRight = () => {
+        let slider = document.getElementById('slider' + rowID);
+        slider.scrollLeft = slider.scrollLeft + 500;
+    }
+
+    console.log(movies)
 
     return (
         <>
             <h2 className='text-white font-bold md:text-xl p-4'>{title}</h2>
-            <div className='relative flex items-center'>
-                <div id={'slider'}>
+            <div className='relative flex items-center group'>
+                <MdChevronLeft onClick={slideLeft} size={40} className='bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block' />
+                <div id={'slider' + rowID} className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative'>
                     {movies.map((item, id) => (
-                        <div className='w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2'>
-                            <img src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`} alt={item?.title} />
-                        </div>
+                        <Movie key={id} item={item} />
                     ))}
                 </div>
+                <MdChevronRight onClick={slideRight} size={40} className='bg-white right-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block' />
             </div>
         </>
     )
